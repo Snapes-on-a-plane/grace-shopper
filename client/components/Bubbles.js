b0859e7ffc67c8e8a9ac084390b9ba43d9269d88
Stacy <stacello19@gmail.com>
2019-09-17 11:56:58 -0400

    Feature: Association and Bubble.js

---
+++ client/components/Bubbles.js
@@ -0,0 +1,54 @@
+//picture, address, name, price, rating, open hour
+import React, {Component} from 'react'
+import {Card, Button} from 'react-bootstrap'
+import {Link} from 'react-router-dom'
+import {connect} from 'react-redux'
+
+//intial: S.C.  Comment: finished the Frontend part for the bubbles also implemented supposed redux (state, dispatch)
+class AllBubble extends Component {
+  render() {
+    const MapBubbles = this.props.bubbles.map(bubble => {
+      return (
+        <div key={bubble.id}>
+          <Card style={{width: '18rem'}}>
+            <Card.Img variant="top" src={bubble.img} />
+            <Card.Body>
+              <Card.Title>{bubble.name}</Card.Title>
+              <Card.Text>
+                Address: {bubble.address}
+                <br />
+                Price : {bubble.price}
+                <br />
+                Open Hour: {bubble.openHr}
+                <br />
+                Rating: {bubble.rating}
+                <br />
+              </Card.Text>
+              <Button variant="primary">
+                <Link to={`/bubbles/${bubble.id}`}>Order Me</Link>
+              </Button>
+            </Card.Body>
+          </Card>
+        </div>
+      )
+    })
+    return (
+      <div>
+        <h2>Bubbles:</h2>
+        <div className="bubbles">{MapBubbles}</div>
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    bubbles: state.bubbles
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {}
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(AllBubble)
--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,18 +1,16 @@
 const User = require('./user')
+const Bubble = require('./bubble');
+const Order = require('./order');
 
-/**
- * If we had any associations to make, this would be a great place to put them!
- * ex. if we had another model called BlogPost, we might say:
- *
- *    BlogPost.belongsTo(User)
- */
+//Initial: S.C. Comment: Associations
+Order.hasMany(Bubble);
+Bubble.belongsTo(Order);
 
-/**
- * We'll export all of our models here, so that any time a module needs a model,
- * we can just require it from 'db/models'
- * for example, we can say: const {User} = require('../db/models')
- * instead of: const User = require('../db/models/user')
- */
+User.hasMany(Order);
+Order.belongsTo(User);
+
 module.exports = {
-  User
+  User,
+  Order,
+  Bubble
 }
