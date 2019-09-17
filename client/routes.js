fa8fca1411d7e900225d2ce1bf4a2fdc210db30e
Nayana Limaye <nalimaye@yahoo.com>
2019-09-17 15:47:11 -0400

    Initial commit for feature-#10

--- client/routes.js
+++ client/routes.js
@@ -2,7 +2,15 @@ import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {withRouter, Route, Switch} from 'react-router-dom'
 import PropTypes from 'prop-types'
-import {Login, Signup, UserHome} from './components'
+import {
+  Login,
+  Signup,
+  UserHome,
+  HomePage,
+  PageNotFound,
+  AllBubble,
+  Order
+} from './components'
 import {me} from './store'
 
 /**
@@ -19,16 +27,18 @@ class Routes extends Component {
     return (
       <Switch>
         {/* Routes placed here are available to all visitors */}
-        <Route path="/login" component={Login} />
-        <Route path="/signup" component={Signup} />
+        {/* NL: Modifying routes to reflect our website schema */}
+        <Route path="/" component={HomePage} />
+        <Route path="/bubbles" component={AllBubble} />
+        <Route path="/order" component={Order} />
         {isLoggedIn && (
           <Switch>
             {/* Routes placed here are only available after logging in */}
             <Route path="/home" component={UserHome} />
           </Switch>
         )}
-        {/* Displays our Login component as a fallback */}
-        <Route component={Login} />
+        {/* Displays our PageNotFound component as a fallback */}
+        <Route component={PageNotFound} />
       </Switch>
     )
   }
