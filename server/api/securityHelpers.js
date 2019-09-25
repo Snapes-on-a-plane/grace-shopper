bee3c27ba5a33287e0583a58d18c6258a9aef1c3
LISA JO <37820396+nottheaveragejo@users.noreply.github.com>
2019-09-25 08:25:12 -0400

    Update securityHelpers.js

--- server/api/securityHelpers.js
+++ server/api/securityHelpers.js
@@ -1,16 +1,5 @@
 function secureRoutes(req, res, next) {
-  if (req.rawHeaders.includes('Referer')) {
-    return next()
-  } else if (req.user) {
-    const admin = req.user.isAdmin
-    if (!req.rawHeaders.includes('Referer') && admin) {
-      return next()
-    } else {
-      res.redirect('/')
-    }
-  } else {
     res.redirect('/')
-  }
 }
 
 module.exports = secureRoutes
